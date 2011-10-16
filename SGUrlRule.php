f56b18aac64c555370b9cb453ec331880aab7e49
lexunin@gmail.com <lexunin@gmail.com@d94edb47-f4e9-22ba-4467-dac2e9e8ade1>
2011-10-16 15:13:54 +0000

    0.8 preversion

--- SGUrlRule.php
+++ SGUrlRule.php
@@ -1,28 +1,36 @@
 <?php
 /**
- * Sitemap generator controllers action
+ * Sitemap generator UrlManager Rule
  * @author Evgeny Lexunin <lexunin@gmail.com>
  * @link http://www.yiiframework.com/extension/sitemapgenerator/
- * @version 0.7a
+ * @version 0.8a
  * @license New BSD
  */
-/*
+/**
  * Configuration:
  * @property string $route Route to controller action for sitemap. Default: /site/sitemap
  */
 class SGUrlRule extends CBaseUrlRule
 {
-	public $route='/site/sitemap';
+	public $route='/sitemap';
 	
 	public function parseUrl($manager, $request, $pathInfo, $rawPathInfo)
 	{
-		if (substr($request->url,0,8)==='/sitemap') {
-			$_GET['mapName']=trim($request->url,'/');
+		$url=$this->trimBaseUrl($request->url);
+		if (substr($url,0,7)==='sitemap') {
+			$_GET['mapName']=$url;
 			return $this->route;
 		} else
 			return false;	
 	}
 	public function createUrl($manager, $route, $params, $ampersand) {
 		return false;
+	}
+	private function trimBaseUrl($url)
+	{
+		$burl=Yii::app()->baseUrl;
+		if (substr($url,0,strlen($burl))===$burl)
+			$url=substr($url,strlen($burl));
+		return trim($url,'/');
 	}
 }
